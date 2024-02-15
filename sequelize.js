4e2dcff7bc6bc19ebc863f1dd69a4cd9d740db3a
lexxwork <lexxwork@gmail.com>
2024-02-15 22:28:37 +0200

    fix env vars

--- sequelize.js
+++ sequelize.js
@@ -1,21 +1,26 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 let sequelize;
 
-if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: true,
-    },
-  });
+if (process.env.NODE_ENV === "production") {
+	sequelize = new Sequelize(
+		process.env.DB_DATABASE,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: process.env.DB_HOST,
+			dialect: "postgres",
+			dialectOptions: {
+				ssl: true,
+			},
+		},
+	);
 } else {
-  const DB_FILE_PATH = './.data/pixel.db';
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: DB_FILE_PATH,
-  });
+	const DB_FILE_PATH = "./.data/pixel.db";
+	sequelize = new Sequelize({
+		dialect: "sqlite",
+		storage: DB_FILE_PATH,
+	});
 }
 
 module.exports = sequelize;
